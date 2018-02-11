9c2abed0fcdae1479622eb8970d18af4fe3e8c13
Sepand Pouyanmehr <sep@Sepands-MBP.home>
2018-02-11 20:44:05 +0000

    Passed Variable

--- lib/main.rb
+++ lib/main.rb
@@ -5,7 +5,7 @@ pizza_height = 0
 pizza_l = 0
 pizza_h = 0
 
-File.open("/Users/sep/Desktop/google_challenge_test/Google-HashCode-2018-Test-Round/inputs/small.in", "r") do |f|
+File.open(ARGV[0], "r") do |f|
 	numbers = f.first.split().map { |chr| chr.to_i  }
 	
 	f.each do |line|
@@ -18,4 +18,5 @@ pizza_width = numbers[0]
 pizza_height = numbers[1]
 pizza_l = numbers[2]
 pizza_h = numbers[3]
+
 
