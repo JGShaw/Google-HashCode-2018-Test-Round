7bd3fec59c8dbc66019a66bdee7de323e7428733
Sepand Pouyanmehr <sep@Sepands-MBP.home>
2018-02-11 20:31:12 +0000

    variables yay

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,21 @@
+lines = []
+numbers = []
+pizza_width = 0
+pizza_height = 0
+pizza_l = 0
+pizza_h = 0
+
 File.open("/Users/sep/Desktop/google_challenge_test/Google-HashCode-2018-Test-Round/inputs/small.in", "r") do |f|
 	numbers = f.first.split().map { |chr| chr.to_i  }
 	
 	f.each do |line|
-		puts line.chars
+		lines << line
 	end
 
 end
+
+pizza_width = numbers[0]
+pizza_height = numbers[1]
+pizza_l = numbers[2]
+pizza_h = numbers[3]
+
