20d3613100a3802540de374b859486300ba39108
Joseph Shaw <j.g.shaw95@gmail.com>
2018-02-11 20:27:07 +0000

    Fix type

--- lib/slice.rb
+++ lib/slice.rb
@@ -1,7 +1,7 @@
 class Slice
   attr_reader :row_start, :col_start, :row_end, :col_end
   
-  def intialize row_start, col_start, row_end, col_end
+  def initialize row_start, col_start, row_end, col_end
     @row_start = row_start
     @col_start = col_start
     @row_end = row_end
